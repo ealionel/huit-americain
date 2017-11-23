5efde8bdc76f859e60be2c1665ec8d9385241ba7
lunix <floretyounesse@gmail.com>
2017-11-23 23:40:06 +0100

    Refactoring d'un bout de code (création objet effetcarte)

--- huit_americain/src/huit_americain/Regle.java
+++ huit_americain/src/huit_americain/Regle.java
@@ -1,6 +1,6 @@
-package fr.lo02.huitamericain;
+package huit_americain;
 
-import fr.lo02.effets.*;
+import effets.*;
 
 /**
  * Classe qui contient les differentes r�gles du jeu.
@@ -14,7 +14,6 @@ public class Regle {
 	protected int nbJeuxCartes;	//Nombre de jeux de cartes, peut �tre choisi en fonction du nombre de joueurs.
 	protected int modePoints; 	//Pour choisir si le nombre de points est � compte positif ou n�gatif
 	protected boolean joker; 	//Si on met un joker ou non dans le d�que
-	protected int nbCartesDebut;//Nombre distribu� � chaque joueur au d�but d'une partie
 	
 	/**
 	 * Cet atribut permet � attribuer � chaque carte, pour chaque variante diff�rente, un effet diff�rent. Par exemple effetCartes[0] est l'effet de l'as, effetCartes[1] du 2, etc...
@@ -22,7 +21,7 @@ public class Regle {
 	protected Effet[] effetCartes = new Effet[14]; //Liste qui va contenir la liste des effets pour chaque valeur de cartes, genre {new EffetNormal, new EffetChangerCouleur, new EffetNormal, ...}, effetCartes[0] correspond � l'effet de l'As, etc.
 	
 	/**
-	 * Regles par défaut du jeu : version minimale.
+	 * Regles par d�faut du jeu : version minimale.
 	 */
 	Regle(int nbJoueurs, int nbJeuxCartes, int modePoints, boolean joker) {
 		
@@ -31,59 +30,13 @@ public class Regle {
 		this.modePoints = modePoints;
 		this.joker = joker;
 		
+		
+		
 		effetCartes[0] = new EffetNormal();
 		effetCartes[1] = new EffetPiocherCarte();
 		
 		for (int i = 2; i < 14; i++) {
 			effetCartes[i] = new EffetNormal();
 	   }
-	}
-
-	public int getNbJoueurs() {
-		return nbJoueurs;
-	}
-
-	public int getNbJeuxCartes() {
-		return nbJeuxCartes;
-	}
-
-	public int getModePoints() {
-		return modePoints;
-	}
-
-	public boolean isJoker() {
-		return joker;
-	}
-
-	public int getNbCartesDebut() {
-		return nbCartesDebut;
-	}
-
-	public Effet[] getEffetCartes() {
-		return effetCartes;
-	}
-
-	public void setNbJoueurs(int nbJoueurs) {
-		this.nbJoueurs = nbJoueurs;
-	}
-
-	public void setNbJeuxCartes(int nbJeuxCartes) {
-		this.nbJeuxCartes = nbJeuxCartes;
-	}
-
-	public void setModePoints(int modePoints) {
-		this.modePoints = modePoints;
-	}
-
-	public void setJoker(boolean hasJoker) {
-		this.joker = hasJoker;
-	}
-
-	public void setNbCartesDebut(int nbCartesDebut) {
-		this.nbCartesDebut = nbCartesDebut;
-	}
-
-	public void setEffetCartes(Effet[] effetCartes) {
-		this.effetCartes = effetCartes;
 	}
 }
